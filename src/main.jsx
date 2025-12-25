772dd87fd23c597f371e4fab9ce0e5ac8cf0e39c
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-12-25 08:50:51 +0500

    feat: add MUI and Emotion dependencies, implement theme provider
    - Added @emotion/react, @emotion/styled, @mui/icons-material, @mui/material, and @mui/styled-engine-sc to dependencies in package.json.
    - Included styled-components for additional styling capabilities.
    - Imported Google Fonts in index.css for typography.
    - Wrapped the App component with MUI's ThemeProvider in main.jsx to apply custom theme.
    - Created a new theme configuration in theme.jsx to set up typography with Lato font.

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme/theme.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>
 );
---
+++ src/theme/theme.jsx
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: "'Lato', sans-serif",
+    
+    // fontSize,
+    // allVariants,
+    // body1,
+    // fontWeightBold,
+
+  },
+});
