161a932d62b2d317b47e202fb2bdfe4dbaf183b5
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-12-28 16:59:17 +0500

    feat: integrate redux-persist for state persistence in Redux store

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { persistor, store } from "./store/store.js";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme/theme.jsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </ThemeProvider>
   </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { questionReducer, resultReducer } from "../features";
 import authReducer from "../features/authSlice.js";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/es/storage";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whiteList: ["auth"]
+}
+
+const rootReducer = combineReducers({
+    auth: authReducer
+})
+
+const persistedReducer = persistReducer(
+    persistConfig,
+    rootReducer
+)
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
+        persistedReducer,
         question: questionReducer,
         result: resultReducer
     },
+
+    middleware: (defaultMiddleware) =>
+        defaultMiddleware({ serializableCheck: false }),
+
+
+
     devTools: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 })
+
+export const persistor = persistStore(store)
