0d3f39e5bed2a8110aedbd3b3d61514321f1ee59
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-12-30 14:13:45 +0500

    feat: enhance Login and Register components with improved UI and form handling

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,59 +1,101 @@
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import { ButtonComp, FormikInput } from "../../components";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../../features/asyncThunk";
 import { useNavigate } from "react-router";
+import { Box, Typography } from "@mui/material";
+// import { useEffect } from "react";
 
 const Login = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const dispatch = useDispatch();
+  // const { auth } = useSelector((state) => state.persistedReducer);
+
+  // useEffect(() => {
+  //   if (auth.isAuthenticated) {
+  //     navigate("/dashboard");
+  //   }
+  // }, []);
+
   return (
-    <div style={{ padding: "20px", maxWidth: "550px", margin: "0 auto" }}>
-      <h1>Log in</h1>
-      <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
-        validationSchema={Yup.object({
-          email: Yup.string()
-            .email("Invalid email format")
-            .required("Email is required"),
-          password: Yup.string()
-            .required("Password is required")
-            .min(8, "Password must be at least 8 characters")
-            .matches(/[a-zA-Z]/, "Password can only contain Latin letters"),
-        })}
-        onSubmit={(userData, { setSubmitting }) => {
-          console.log("Submit", userData);
-
-          dispatch(loginUser(userData));
-          setSubmitting(false);
-          navigate("/dashboard");
-          // setTimeout(() => {
-          //   alert(JSON.stringify(userData, null, 2));
-
-          // }, 500);
+    <Box
+      sx={{
+        minHeight: "50vh",
+        display: "flex",
+        textAlign: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "lato, sans-serif",
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: 420,
+          p: 4,
+          boxShadow: 3,
+          borderRadius: 2,
+          textAlign: "center",
+          alignItems: "center",
+          bgcolor: "background.paper",
         }}
       >
-        {({ isSubmitting, handleSubmit }) => (
-          <Form onSubmit={handleSubmit}>
-            <FormikInput name="email" label="Email" type="email" />
+        <Typography variant="h5" fontWeight="bold" textAlign="center" mb={3}>
+          Welcome back 👋
+        </Typography>
+        <p>Log in to continue learning and track your progress</p>
+        <Box sx={{ p: 1, mt: 2}}>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={Yup.object({
+              email: Yup.string()
+                .email("Invalid email format")
+                .required("Email is required"),
+              password: Yup.string()
+                .required("Password is required")
+                .min(8, "Password must be at least 8 characters")
+                .matches(/[a-zA-Z]/, "Password can only contain Latin letters"),
+            })}
+            onSubmit={(userData, { setSubmitting }) => {
+              console.log("Submit", userData);
 
-            <FormikInput name="password" label="Password" type="password" />
-
-            <div style={{ marginTop: "16px" }}>
-              <ButtonComp
-                containedValue="Submit"
-                type="submit"
-                disabled={isSubmitting}
-              />
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
+              dispatch(loginUser(userData));
+              setSubmitting(false);
+            }}
+          >
+            {({ isSubmitting, handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
+                <Box mb={2}>
+                  <FormikInput name="email" label="Email" type="email" />
+                </Box>
+                <Box mb={2}>
+                  <FormikInput
+                    name="password"
+                    label="Password"
+                    type="password"
+                  />
+                </Box>
+                <div style={{ marginTop: "16px" }}>
+                  <ButtonComp
+                    containedValue="Login"
+                    type="submit"
+                    disabled={isSubmitting}
+                    fullWidth
+                  />
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+         <Typography variant="body2" textAlign="center" mt={3} color="gray">
+          New to Quizora? <a href="/register" className="text-blue-700 bold"> Sign up</a>
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -3,57 +3,97 @@ import * as Yup from "yup";
 import { ButtonComp, FormikInput } from "../../components";
 import { useDispatch } from "react-redux";
 import { registerUser } from "../../features/asyncThunk";
+import { Box, Typography } from "@mui/material";
 
 const Register = () => {
   const dispatch = useDispatch();
   return (
-    <div style={{ padding: "20px", maxWidth: "500px", margin: "0 auto" }}>
-      <h1>Sign Up</h1>
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-          password: "",
-        }}
-        validationSchema={Yup.object({
-          firstName: Yup.string().required("First name is required"),
-          lastName: Yup.string().required("Last name is required"),
-          email: Yup.string()
-            .email("Invalid email format")
-            .required("Email is required"),
-          password: Yup.string()
-            .required("Password is required")
-            .min(8, "Password must be at least 8 characters")
-            .matches(/[a-zA-Z]/, "Password can only contain Latin letters"),
-        })}
-        onSubmit={(userData, { setSubmitting }) => {
-          console.log("Submit", userData);
-          dispatch(registerUser(userData));
-          setSubmitting(false);
+    <Box
+      sx={{
+        minHeight: "60vh",
+        display: "flex",
+        textAlign: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "lato, sans-serif",
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: 420,
+          p: 4,
+          boxShadow: 3,
+          borderRadius: 2,
+          textAlign: "center",
+          alignItems: "center",
+          bgcolor: "background.paper",
         }}
       >
-        {({ isSubmitting, handleSubmit }) => (
-          <Form onSubmit={handleSubmit}>
-            <FormikInput name="firstName" label="First Name" />
-
-            <FormikInput name="lastName" label="Last Name" />
-
-            <FormikInput name="email" label="Email" type="email" />
-
-            <FormikInput name="password" label="Password" type="password" />
-
-            <div style={{ margin: "20px" }}>
-              <ButtonComp
-                containedValue={isSubmitting ? "Submitting..." : "Submit"}
-                type="submit"
-                disabled={isSubmitting}
-              />
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
+        <Typography variant="h5" fontWeight="bold" textAlign="center" mb={3}>
+          Sign Up
+        </Typography>
+        <p>Join Quizora today and start practicing smarter, not harder. </p>
+        <Box sx={{ p: 1 }}>
+          <Formik
+            initialValues={{
+              firstName: "",
+              lastName: "",
+              email: "",
+              password: "",
+            }}
+            validationSchema={Yup.object({
+              firstName: Yup.string().required("First name is required"),
+              lastName: Yup.string().required("Last name is required"),
+              email: Yup.string()
+                .email("Invalid email format")
+                .required("Email is required"),
+              password: Yup.string()
+                .required("Password is required")
+                .min(8, "Password must be at least 8 characters")
+                .matches(/[a-zA-Z]/, "Password can only contain Latin letters"),
+            })}
+            onSubmit={(userData, { setSubmitting }) => {
+              console.log("Submit", userData);
+              dispatch(registerUser(userData));
+              setSubmitting(false);
+            }}
+          >
+            {({ isSubmitting, handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
+                <Box mb={2}>
+                  <FormikInput name="firstName" label="First Name" fullWidth />
+                </Box>
+                <Box mb={2}>
+                  <FormikInput name="lastName" label="Last Name" fullWidth />
+                </Box>
+                <Box mb={2}>
+                  <FormikInput name="email" label="Email" type="email" />
+                </Box>
+                <Box mb={2}>
+                  <FormikInput
+                    name="password"
+                    label="Password"
+                    type="password"
+                  />
+                </Box>
+                <div style={{ margin: "20px" }}>
+                  <ButtonComp
+                    containedValue={isSubmitting ? "Submitting..." : "Sign up"}
+                    type="submit"
+                    disabled={isSubmitting}
+                    fullWidth
+                  />
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+        <Typography variant="body2" textAlign="center" mt={1} color="gray">
+          Already have an account? <a href="/login"  className="text-blue-700 bold">Log in</a>
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
